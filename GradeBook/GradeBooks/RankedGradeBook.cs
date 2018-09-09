00a9fa7c3b291ae7f62b718ca1bddff226f03f79
Marloes <marloes.de.krom@gmail.com>
2018-09-09 13:00:09 +0200

    completed the module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -48,7 +48,7 @@ namespace GradeBook.GradeBooks
         {
             if (Students.Count < 5)
             {
-                Console.WriteLine("anked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
                 return;
             }
             base.CalculateStudentStatistics(name);
